61d68b875b0b00bf186802df15757f3d651c5981
duanzin <lucaduan@gmail.com>
2023-03-07 14:45:29 -0300

    chore: arquitetura basica feita

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GlobalStyle from "./globalStyle";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Context } from "./contexts/Context";
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Context.Provider value={{ }}>
+        <Routes>
+        </Routes>
+      </Context.Provider>
+    </BrowserRouter>
   );
 }
-
-export default App;
