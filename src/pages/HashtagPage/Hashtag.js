b11603d8f3fbd8cfa8f7e7705045a53852b6b4f1
duanzin <lucaduan@gmail.com>
2023-03-07 17:47:40 -0300

    feat: renderiza as tags na sidebar de forma dinamica

--- src/pages/HashtagPage/Hashtag.js
+++ src/pages/HashtagPage/Hashtag.js
@@ -1,25 +1,29 @@
-import { StyledAside } from './style';
+import { StyledAside } from "./style";
 import React, { useContext } from "react";
-import styled from "styled-components";
 import axios from "axios";
 import LiHashtags from "./LiHashtags";
 
 export default function Hashtag() {
-    return (
-        <StyledAside>
-          <h2>trending</h2>
-          <ul>
-            <li># java</li>
-            <li># react</li>
-            <li># grana</li>
-            <li># money</li>
-            <li># teste</li>
-            <li># front</li>
-            <li># git</li>
-            <li># hub</li>
-            <li># driven</li>
-            <li># ps4</li>
-          </ul>
-        </StyledAside>
-      );
+  const tags = [
+    "javascript",
+    "react",
+    "css",
+    "node",
+    "sql",
+    "mongodb",
+    "drive",
+    "hub",
+    "ps4",
+    "teste",
+  ];
+  return (
+    <StyledAside>
+      <h2>trending</h2>
+      <ul>
+        {tags.map((tag) => (
+          <LiHashtags key={tag} tag={tag} />
+        ))}
+      </ul>
+    </StyledAside>
+  );
 }
--- src/pages/HashtagPage/LiHashtags.js
+++ src/pages/HashtagPage/LiHashtags.js
@@ -1,3 +1,8 @@
-export default function LiHashtags(){
-    
+import React from "react";
+import axios from "axios";
+
+export default function LiHashtags(props){
+    return(
+        <li onClick={() => {}}># {props.tag}</li>
+    )
 }
