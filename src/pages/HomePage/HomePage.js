253d2dd466fea9a01335ac8dd39d3122c599fa12
MoniqueSouza <moniquemoreira.mkt@gmail.com>
2023-03-08 11:09:43 -0300

    feat: Layout Estático

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,39 @@
-import { } from './style'
+import {Left, All, Right, StyledForm, StyledInput, StyledButton, StyledLink } from './style'
 
 export default function HomePage() {
     return (
-        <div>HomePage</div>
+        <All>
+            <Left>
+                <h1>linkr</h1>
+                <h2>save, share and discover</h2>
+                <h2> the best links on the web</h2>
+            </Left>
+            <Right>
+                <StyledForm>
+
+                    <StyledInput
+                        name="e-mail"
+                        placeholder="e-mail"
+                        type="email"
+                        required
+                    />
+                    <StyledInput
+                        name="password"
+                        placeholder="password"
+                        type="password"
+                        required
+                    />
+                    
+                    <StyledButton>
+                        Log In
+                    </StyledButton>
+
+                </StyledForm>
+
+                <StyledLink to="/signup">
+                First time? Create an account!
+                </StyledLink>
+            </Right>
+        </All>
     )
 }
